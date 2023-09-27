0c528f3bd3e845e7f6ddf6ebbb4bf11239b8ab6b
Tuan Anh Nguyen <atngu312@gmail.com>
2023-09-27 01:26:20 -0400

    remove sample data to parents App.jsx / add html elements with corresponding class name with the given Scss styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
+const PhotoListItem = (props) => {
+  return (
+    <div className='photo-list__item'>
+      <PhotoFavButton key={props.id}/>
+      <img className='photo-list__image' src={props.imageSource} />
+      <div className='photo-list__user-details'>
+        <img className='photo-list__user-profile' src={props.profile} />
+        <div className='photo-list__user-info'>
+          {props.username}
+          <div className='photo-list__user-location'>{`${props.city}, ${props.country}`}</div>
+        </div>
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
