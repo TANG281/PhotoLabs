2f339624212d9712d27892403325d9261a3f95f3
Tuan Anh Nguyen <atngu312@gmail.com>
2023-09-27 01:24:37 -0400

    display 3 identical mock photos, passing data as props to PhotoListItem component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,39 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import PhotoFavButton from 'components/PhotoFavButton';
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
+const listOfPhotos = [];
+for (let i = 0; i < 3; i++) {
+  listOfPhotos.push(
+      <PhotoListItem
+        key={sampleDataForPhotoListItem.id}
+        city={sampleDataForPhotoListItem.location.city}
+        country={sampleDataForPhotoListItem.location.country}
+        imageSource={sampleDataForPhotoListItem.imageSource}
+        username={sampleDataForPhotoListItem.username}
+        profile={sampleDataForPhotoListItem.profile}
+      />
+  );
+}
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+
+      {listOfPhotos}
     </div>
   );
 };
