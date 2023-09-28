d324e773e91a527dbd024f5dfefedb4241041959
Tuan Anh Nguyen <atngu312@gmail.com>
2023-09-28 16:27:19 -0400

    create favs state and toggleFav function and pass them down as props to children components

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -7,10 +7,17 @@ import PhotoList from 'components/PhotoList';
 
 const HomeRoute = (props) => {
 
+  const [favs, setFavs] = useState([]);
+
+  const toggleFav = (photoId) => {
+    const newFavs = [...favs];
+    favs.includes(photoId) ? newFavs.splice(favs.indexOf(photoId),1) && setFavs(newFavs) : newFavs.push(photoId) && setFavs(newFavs)
+  };
+
   return (
     <div className="home-route">
-      <TopNavigation topics={props.topics} isFavPhotoExist={isFavPhotoExist} />
-      <PhotoList photos={props.photos} swithFavExistance={swithFavExistance}/>
+      <TopNavigation topics={props.topics} favs={favs} />
+      <PhotoList photos={props.photos} favs={favs} toggleFav={toggleFav} />
     </div>
   );
 };
