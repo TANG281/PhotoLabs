c80599a3cd3f6aa6da5b1f091fc129d3b5f20cb3
Tuan Anh Nguyen <atngu312@gmail.com>
2023-09-28 23:30:08 -0400

    move all states to App and render PhotoDetailsModal with conditioning

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => setIsOpen(!isOpen);
+
+  const [chosenPhoto, setChosenPhoto] = useState({});
+  const choosePhoto = (photo) => setChosenPhoto(photo);
+
+  const [favs, setFavs] = useState([]);
+  const toggleFav = (photoId) => {
+    const newFavs = [...favs];
+    favs.includes(photoId) ? newFavs.splice(favs.indexOf(photoId), 1) && setFavs(newFavs) : newFavs.push(photoId) && setFavs(newFavs);
+  };
+
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute
+        photos={photos}
+        topics={topics}
+        isOpen={isOpen}
+        toggleModal={toggleModal}
+        choosePhoto={choosePhoto}
+        favs={favs}
+        toggleFav={toggleFav}
+      />
+      {isOpen &&
+        <PhotoDetailsModal
+          toggleModal={toggleModal}
+          chosenPhoto={chosenPhoto}
+          choosePhoto={choosePhoto}
+          favs={favs}
+          toggleFav={toggleFav}
+        />}
     </div>
   );
 };
