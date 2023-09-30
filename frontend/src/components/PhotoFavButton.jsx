d55562a6ce05b5805cc2d97f9b1ec0caecc38d6c
Tuan Anh Nguyen <atngu312@gmail.com>
2023-09-30 14:51:59 -0400

    update linting

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,14 @@ import React, { useCallback, useState } from 'react';
 import '../styles/PhotoFavButton.scss';
 import FavIcon from './FavIcon';
 
-function PhotoFavButton({favs, photoId, toggleFav}) {
+function PhotoFavButton({ favs, photoId, toggleFav }) {
 
   const colorCheck = favs.includes(photoId);
-  
+
   return (
     <div onClick={() => toggleFav(photoId)} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <FavIcon selected={colorCheck}/>
+        <FavIcon selected={colorCheck} />
       </div>
     </div>
   );
