d0f2082073c7faf2e92211cd924186d5d84d53a8
Tuan Anh Nguyen <atngu312@gmail.com>
2023-10-01 15:45:43 -0400

    remove hardcoded mock data and get data from backend through state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from './hooks/useApplicationData';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const { state, toggleModal, choosePhoto, toggleFav } = useApplicationData();
-  const { isModalOpen, chosenPhoto, favs } = state;
+  const { state, toggleModal, choosePhoto, toggleFav, getPhotosByTopic } = useApplicationData();
+  const { isModalOpen, chosenPhoto, favs, photoData, topicData } = state;
 
   return (
     <div className="App">
       <HomeRoute
-        photos={photos}
-        topics={topics}
+        photos={photoData}
+        topics={topicData}
         isModalOpen={isModalOpen}
         toggleModal={toggleModal}
         choosePhoto={choosePhoto}
         favs={favs}
         toggleFav={toggleFav}
+        getPhotosByTopic={getPhotosByTopic}
       />
       {isModalOpen &&
         <PhotoDetailsModal
